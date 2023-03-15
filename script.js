ec9e08bc9400ab940ff4b652acd0e9076bc45830
Elias <eliasmazo@hotmail.com>
2023-03-15 15:29:13 -0500

    Agregando index, style y script

---
+++ script.js
@@ -0,0 +1,13 @@
+let link = document.querySelector(".button");
+
+
+
+button.addEventListener("mouseover", e => {
+    e.target.style.color = "hsl(219, 85%, 26%)";
+});
+
+button.addEventListener("mouseout", e => {
+    e.target.style.color = "hsl(219, 12%, 42%)";
+});
+
+
